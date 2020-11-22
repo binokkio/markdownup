c47fcb83178d657d2b27753daa0ee95eded54781
Binokkio <binokkio@b.nana.technology>
2020-11-22 19:27:12 +0100

    Added pygments as an install requirement and removed obsolete requirements.txt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='markdownup',
     description='Markdown server',
     keywords=['markdown', 'server'],
-    version='0.0.3',
+    version='0.0.4',
     author='Binokkio',
     author_email='binokkio@b.nana.technology',
     url='https://github.com/binokkio/markdownup',
@@ -16,6 +16,7 @@ setuptools.setup(
         'chevron',
         'gunicorn',
         'markdown',
+        'pygments',
         'pyyaml'
     ]
 )
