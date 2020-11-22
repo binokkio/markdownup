e55209e6d84e1ffc85351f47c39fa5d65a43f14d
Binokkio <binokkio@b.nana.technology>
2020-11-22 13:57:13 +0100

    Prepared for upload to pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,19 @@ import setuptools
 
 setuptools.setup(
     name='markdownup',
+    description='Markdown server',
+    keywords=['markdown', 'server'],
     version='0.0.1',
+    author='Binokkio',
+    author_email='binokkio@b.nana.technology',
+    url='https://github.com/binokkio/markdownup',
+    license='LGPLv3+',
     packages=setuptools.find_packages(),
     package_dir={'': 'src'},
-    include_package_data=True
+    include_package_data=True,
+    install_requires=[
+        'chevron',
+        'gunicorn',
+        'markdown'
+    ]
 )
