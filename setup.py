9a015d4541a57d647a2c05a879fe5bdd54c634f7
Binokkio <binokkio@b.nana.technology>
2020-12-04 21:05:31 +0100

    Bumped patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='markdownup',
     description='Markdown server',
     keywords=['markdown', 'server'],
-    version='0.0.13',
+    version='0.0.14',
     author='Binokkio',
     author_email='binokkio@b.nana.technology',
     url='https://github.com/binokkio/markdownup',
